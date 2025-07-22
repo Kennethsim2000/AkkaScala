1ef62a47a1774188b0b9ad529237a85be051ee3e
kenneth <kenneth12345645@gmail.com>
2025-07-22 18:29:21 +0800

    Add in passing message to another actor and receiving a response

--- src/main/scala/askpattern/AskPattern.scala
+++ src/main/scala/askpattern/AskPattern.scala
@@ -1,19 +1,23 @@
 package askpattern
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.pattern._
 import akka.util.Timeout
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 
 object AskPattern extends App {
     case object AskName //when we have no parameters, we use a case object instead
     case class NameResponse(name: String)
+    case class AskNameOf(other: ActorRef)
 
     val system = ActorSystem("system")
-    val actor = system.actorOf(Props(new AskPatternActor("name")), "askActor1")
+    val actor1 = system.actorOf(Props(new AskPatternActor("name")), "askActor1")
+    val actor2 = system.actorOf(Props(new AskPatternActor("name2 ")), "askActor2")
+
     implicit val timeout = Timeout(1.seconds)
-    val answer = actor ? AskName //import akka.pattern._ to import the ?
+    val answer = actor1 ? AskName //import akka.pattern._ to import the ?
     //requires an implicit timeout variable
     // our answer here is a future,
 
@@ -21,5 +25,7 @@ object AskPattern extends App {
         case NameResponse(name) => println("name is " + name)
         case other => println("Unexpected response " + other)
     }
+
+    actor1 ! AskNameOf(actor2)
     system.terminate()
 }
--- src/main/scala/askpattern/AskPatternActor.scala
+++ src/main/scala/askpattern/AskPatternActor.scala
@@ -1,11 +1,34 @@
 package askpattern
 
 import akka.actor.Actor
-import askpattern.AskPattern.{AskName, NameResponse}
+import akka.pattern._
+import akka.util.Timeout
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import askpattern.AskPattern.{AskName, AskNameOf, NameResponse}
+
+import scala.concurrent.Future
+import scala.concurrent.duration.{DurationDouble, DurationInt}
+import scala.util.{Failure, Success}
 
 class AskPatternActor(val name:String) extends Actor{
 
     override def receive: Receive = {
         case AskName => sender ! NameResponse(name)
+        case AskNameOf(other) =>
+            implicit val timeout = Timeout(1.seconds)
+            val f = other ? AskName
+            f.onComplete {
+                case Success(NameResponse(name)) =>
+                    println("They said their name was " + name)
+                case Success(n) => println("They didnt tell us their name")
+                case Failure(ex) => println("Asking their name failed")
+            }
+            val currentSender = sender
+            Future { // we use a future if we want the actor to do something else and not wait for this
+                // for futures like this, since the actor may receive multiple messages, it is advisable
+                // to store the currentSender in a variable first, to preserve the sender we want to send the message to.
+                currentSender ! "message"
+            }
     }
 }
